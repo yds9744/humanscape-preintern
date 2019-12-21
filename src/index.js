995187d19db6849a9b021927c29c355ed2659ba5
jinsun <yds9744>
2019-12-21 23:21:15 +0900

    using router

--- src/index.js
+++ src/index.js
@@ -1,27 +1,37 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
+const api = require('./api');
 
-app.use(async (ctx, next) => {
-	console.log(1);
-	const started = new Date();
-	await next();
-	console.log(new Date - started + 'ms');
-	/*(next().then(() => {
-		console.log(new Date() - started + 'ms');
-	});*/
+router.use('/api', api.routes());
+
+router.get('/', (ctx, next) => {
+	ctx.body = '홈';
 });
 
-app.use((ctx, next) => {
-	console.log(2);
-	next();
+router.get('/about',(ctx, next) => {
+	ctx.body = '소개';
 });
 
-app.use(ctx => {
-	ctx.body = 'Hello Koa';
+router.get('/about/:name', (ctx, next) => {
+	const { name } = ctx.params;
+	ctx.body = name + '의 소개';
 });
+
+router.get('/post', (ctx, next) => {
+	const { id } = ctx.request.query;
+	if(id) {
+		ctx.body = '포스트 #' + id;
+	} else {
+		ctx.body = '포스트 아이디가 없습니다.';
+	}
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
 	console.log('jinsun server is listening to port 4000');
 });
-
 
